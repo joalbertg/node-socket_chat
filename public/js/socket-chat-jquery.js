3ebb2f427377b66655d60471ec045f85e6ebf959
Joalbert <joalbertgonzalez@gmail.com>
2020-06-14 13:17:01 -0500

    [ADD]🚀 getAttribute data-id added

--- public/js/socket-chat-jquery.js
+++ public/js/socket-chat-jquery.js
@@ -3,25 +3,35 @@ const params = new URLSearchParams(window.location.search);
 const divUsuarios = document.getElementById('divUsuarios');
 
 const renderToUsers = users => { // [{}, {}, {}]
-  console.log(users);
-
+  const liHead = document.createElement('li');
   let html =`
-  <li>
     <a href="javascript:void(0)" class="active">Chat de <span>${params.get('room')}</span>
-    </a>
-  </li>`;
+    </a>`;
+
+  liHead.innerHTML = html;
+  divUsuarios.innerHTML = liHead;
 
   for(let i = 0; i < users.length; i++) {
-    html += `
-    <li>
+    const li = document.createElement('li');
+    const anchor = `
       <a data-id="${users[i].id}" href="javascript:void(0)">
         <img src="assets/images/users/1.jpg" alt="user-img" class="img-circle">
         <span>
           ${users[i].name}<small class="text-success">online</small>
         </span>
-      </a>
-    </li>`;
+      </a>`;
+    li.innerHTML = anchor;
+    setEvent(li);
+    divUsuarios.append(li);
   }
-  divUsuarios.innerHTML = html;
+  //divUsuarios.innerHTML = html;
+}
+
+// Listeners
+const setEvent = li => {
+  li.addEventListener('click', event => {
+    console.log(event.currentTarget.firstElementChild);
+    const id event.currentTarget.firstElementChild.getAttribute;
+  });
 }
 
