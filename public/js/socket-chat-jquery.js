abe83f995671311d944ab18134f471fbe45bd0e9
Joalbert <joalbertgonzalez@gmail.com>
2020-06-14 11:08:56 -0500

    [ADD]🚀 assets and renderToUsers left added

---
+++ public/js/socket-chat-jquery.js
@@ -0,0 +1,27 @@
+const params = new URLSearchParams(window.location.search);
+
+const divUsuarios = document.getElementById('divUsuarios');
+
+const renderToUsers = users => { // [{}, {}, {}]
+  console.log(users);
+
+  let html =`
+  <li>
+    <a href="javascript:void(0)" class="active">Chat de <span>${params.get('room')}</span>
+    </a>
+  </li>`;
+
+  for(let i = 0; i < users.length; i++) {
+    html += `
+    <li>
+      <a data-id="${users[i].id}" href="javascript:void(0)">
+        <img src="assets/images/users/1.jpg" alt="user-img" class="img-circle">
+        <span>
+          ${users[i].name}<small class="text-success">online</small>
+        </span>
+      </a>
+    </li>`;
+  }
+  divUsuarios.innerHTML = html;
+}
+
--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,5 +1,5 @@
 const socket = io();
-const params = new URLSearchParams(window.location.search);
+//const params = new URLSearchParams(window.location.search);
 
 if(!(params.has('name') && params.get('name') !== '') || !(params.has('room') && params.get('room') !== '')) {
   window.location = 'index.html';
@@ -15,13 +15,13 @@ socket.on('connect', () => {
   console.log('Server connected');
   socket.emit('joinChat', user, (error, resp) => {
     if (error) throw error;
-    console.log('People', resp);
+    renderToUsers(resp);
   });
 });
 
 socket.on('createMessage', msg => console.log(msg));
 socket.on('userJoin', msg => console.log(msg));
-socket.on('allUsers', users => console.log(users));
+socket.on('allUsers', users => renderToUsers(users));
 
 // private message
 socket.on('privateMessage', message => {
